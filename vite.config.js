1b15008e17d96dbf01009c9debe71b224df0f3b4
adityarwt1 <adityarawatnew2487@gmail.com>
2025-01-01 13:28:45 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AdityaPortfolio/"
-})
+  base: '/adityaportfolio2025/', // Replace <REPO_NAME> with your GitHub repository name
+});
